0e0fb4096b00cd4f3e6c989c3d5fab686ceb54af
Bomi <bomiishere@gmail.com>
2024-06-19 21:40:53 +0800

    Initial Commit

---
+++ Glossika-Assignment/Glossika_AssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Glossika_AssignmentApp.swift
+//  Glossika-Assignment
+//
+//  Created by Bomi Chen on 2024/6/19.
+//
+
+import SwiftUI
+
+@main
+struct Glossika_AssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
